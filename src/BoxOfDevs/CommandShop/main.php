a385bff57e58988041291a2c18c11d05d19d07d3
HimbeersaftLP <mail@himbeer.me>
2017-02-07 21:35:23 +0100

    Added some code (plugin still not finished, DON'T test it)

--- src/BoxOfDevs/CommandShop/main.php
+++ src/BoxOfDevs/CommandShop/main.php
@@ -6,7 +6,7 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\command\ConsoleCommandSender;
 use pocketmine\utils\Config;
-use pocketmine\utils\TextFormat;
+use pocketmine\utils\TextFormat as TF;
 use pocketmine\Server;
 use pocketmine\Player;
 use pocketmine\event\player\PlayerInteractEvent;
@@ -18,6 +18,8 @@ use onebone\economyapi\EconomyAPI;
 
 class main extends PluginBase implements Listener{
      
+     const PREFIX = TF::YELLOW . "[CommandShop]" . TF::WHITE . " ";
+
      public function onEnable(){
           $this->getServer()->getPluginManager()->registerEvents($this,$this);
           $this->getLogger()->info("CommandShop by BoxOfDevs enabled!");
@@ -35,7 +37,20 @@ class main extends PluginBase implements Listener{
      public function onCommand(CommandSender $sender, Command $command, $label, array $args){
           switch($command->getName()){
                case "cshop":
-                    // do stuff, will code when I have time
+                    $subcmd = strtolower(array_shift($args));
+                    switch($subcmd){
+                         case "add":
+                              if(count($args) <= 2) return false;
+                              $name = strtolower(array_shift($args));
+                              $cmd = strtolower(implode(" ", $args));1
+                              $cmds = $this->getConfig()->get("commands", []);
+                              $cmds[$name]["cmd"] = $cmd;
+                              $this->getConfig()->set("commands", $cmds);
+                              $sender->sendMessage(self::PREFIX . TF::GREEN . "Command $name has been successfully added to the list of buyable commands!");
+                              $sender->sendMessage(self::PREFIX . TF::AQUA . "Infos:");
+                              $sender->sendMessage(self::PREFIX . "Name: " . $name);
+                              $sender->sendMessage(self::PREFIX . "Command: " . $cmd);
+                    }
                case "buycmd":
                     // do stuff, will cdoe when i have time
           }
