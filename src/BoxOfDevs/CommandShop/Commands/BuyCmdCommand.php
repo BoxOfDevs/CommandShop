0b7f096d0bf33675e25b2b56c32329c7041670af
HimbeersaftLP <mail@himbeer.me>
2019-05-03 23:54:09 +0200

    Rewrite /buycmd, remove unused functions from main class

--- src/BoxOfDevs/CommandShop/Commands/BuyCmdCommand.php
+++ src/BoxOfDevs/CommandShop/Commands/BuyCmdCommand.php
@@ -5,7 +5,11 @@ declare(strict_types=1);
 namespace BoxOfDevs\CommandShop\Commands;
 
 use BoxOfDevs\CommandShop\CommandShop;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 use pocketmine\command\PluginCommand;
+use pocketmine\network\mcpe\protocol\types\CommandData;
+use pocketmine\Player;
 use pocketmine\plugin\Plugin;
 
 class BuyCmdCommand extends PluginCommand {
@@ -18,5 +22,29 @@ class BuyCmdCommand extends PluginCommand {
           $this->setUsage("/buycmd <command>");
           $this->setPermission("cshop.buy.command");
           $this->plugin = $owner;
+     }
+
+     public function execute(CommandSender $sender, string $commandLabel, array $args) {
+          if(!$sender instanceof Player){
+               $sender->sendMessage(CommandShop::ERROR . "Please use this command in-game!");
+               return true;
+          }
+          if(count($args) < 1) {
+               $sender->sendMessage($this->getUsage());
+               return false;
+          }
+          $cmdName = strtolower(array_shift($args));
+          $cmd = $this->plugin->getCSCommand($cmdName);
+          // TODO: Add translations
+          if ($cmd === null) {
+               $sender->sendMessage(CommandShop::ERROR . "This command wasn't found in the list of buyable commands!");
+          } elseif (!$cmd->getBuyCmdEnabled()) {
+               $sender->sendMessage(CommandShop::ERROR . "/buycmd is disabled for this buyable command!");
+          } elseif (!$cmd->buy($sender)) {
+               $sender->sendMessage(CommandShop::ERROR . "You can't buy this command.");
+          } else {
+               $sender->sendMessage(CommandShop::PREFIX . "You successfully bought the command $cmdName!");
+          };
+          return true;
      }
 }
