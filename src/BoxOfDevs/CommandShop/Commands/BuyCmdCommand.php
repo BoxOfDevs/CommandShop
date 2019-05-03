0c5c438a8eef136501f41e84dc6b59ef87a9ecbd
HimbeersaftLP <mail@himbeer.me>
2019-05-03 22:48:25 +0200

    Start with /buycmd

--- src/BoxOfDevs/CommandShop/Commands/BuyCmdCommand.php
+++ src/BoxOfDevs/CommandShop/Commands/BuyCmdCommand.php
@@ -1,11 +1,22 @@
 <?php
-/**
- * (c)2019 HimbeersaftLP
- */
+
+declare(strict_types=1);
 
 namespace BoxOfDevs\CommandShop\Commands;
 
+use BoxOfDevs\CommandShop\CommandShop;
+use pocketmine\command\PluginCommand;
+use pocketmine\plugin\Plugin;
 
-class BuyCmdCommand {
+class BuyCmdCommand extends PluginCommand {
+     /** @var CommandShop */
+     private $plugin;
 
+     public function __construct(Plugin $owner) {
+          parent::__construct("buycmd", $owner);
+          $this->setDescription('"Buy" a command');
+          $this->setUsage("/buycmd <command>");
+          $this->setPermission("cshop.buy.command");
+          $this->plugin = $owner;
+     }
 }
